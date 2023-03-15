4552c404d8de28f065e6bb19d57d90255a23d90e
Parker Barnett <parkerbarnett02@gmail.com>
2023-03-15 10:25:41 -0400

    Finished Homework exercise

---
+++ script.js
@@ -0,0 +1,52 @@
+// 1
+console.log(7);
+
+// 2
+// Boolean, String, Number, Null, Undefined
+
+// 3
+// +, -, *, /, **, %
+
+// 4
+console.log(5 % 2);
+console.log(5 % 4);
+
+// 5
+console.log('I\'M Marry Poppins y\'All');
+
+// 6
+console.log("Hawk" + "Eye");
+
+// 7
+console.log("Parker".length + "Barnett".length)
+
+// 8
+"Avengers: Endgame"[16];
+
+// BONUS
+
+// 1
+console.log("\\");
+
+// 2
+console.log(5 ** 4 - 9);
+
+// 3
+console.log((("I".length + "AM".length) % "IRON".length) % "MAN".length)
+
+// 4
+console.log("CAP" [1]); 
+console.log("VISION" [0]); 
+console.log("OKOYE"[4]); 
+console.log("NICK FURY" [0]); 
+console.log("GROOT"[0]); 
+console.log("DOCTOR STRANGE" [13]); 
+console.log("THOR" [3]); 
+console.log("STAR-LORD" [0]);
+
+// 5
+var m = 25;
+var c = 4;
+var u = m + c
+console.log(u);
+
